d09bafa7973cff2d04395302e8b1baf86dd9a09e
Pratheba <thedevthoughts@gmail.com>
2022-09-18 22:52:57 +0530

    second commit

---
+++ database/seeders/userSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class userSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'first_name' => 'BalaSubramaniam',
+            'last_name' => 'P',
+            'user_name' => 'bala',
+            'email' => 'bala006@gmail.com',
+            'mobile' => '1234567890',
+            'password' => Hash::make('12345678'),
+            'user_role' => 'admin',
+
+        ]);
+    }
+}
